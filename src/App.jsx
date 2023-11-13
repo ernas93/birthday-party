7d80685b5be31993a63674e006df2b7ef8048fc6
ernas93 <107477572+ernas93@users.noreply.github.com>
2023-11-13 23:52:21 +0100

    setting up styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,22 @@ import './App.css';
 function App() {
   return (
     <main>
+      <img className="hero-image" src="/party.jpeg"></img>
       <h1>Birthday</h1>
+      <h2>Description</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Turpis egestas
+        pretium aenean pharetra magna ac placerat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Turpis egestas
+        pretium aenean pharetra magna ac placerat.
+      </p>
+      <h3>Time and place:</h3>
+      <p>December 15th 8pm at Filmkunstbar</p>
+      <h3>Be there or be square</h3>
     </main>
   );
 }
