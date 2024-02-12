14ff52a67201572e30b9eff5abad229f17ded5f0
ernas93 <107477572+ernas93@users.noreply.github.com>
2024-02-12 17:09:07 +0100

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <div className="hero">
         <img className="hero-image" src={image}></img>
-        <h1 className="main-heading">Dirty Thirties Birthday Party</h1>
+        <h1 className="main-heading">Thirties Birthday Party</h1>
         <h1 className="main-heading">30</h1>
       </div>
       <main>
@@ -15,10 +15,10 @@ function App() {
         <p>
           And the time has come, even for me to admit "defeat" and enter the
           thirties! So I want to celebrate joining the Thirties club, and I
-          wanna celebrate it hard and dirty. I am hoping that every one of you
-          personally can join me and celebrate with me! Celebrate life with me,
-          drink the night, dance out the music, talk out the meanings to this
-          all, catch up, and some of you even meet! Just celebrate!
+          wanna celebrate it. I am hoping that every one of you personally can
+          join me and celebrate with me! Celebrate life with me, drink the
+          night, dance out the music, talk out the meanings to this all, catch
+          up, and some of you even meet! Just celebrate!
         </p>
         <p>Enough of this lameness! LET'S PARTY! Here are the details</p>
         First put on whatever you feel fabulous in!
@@ -36,14 +36,10 @@ function App() {
           <br />
           8pm or 20:00
           <br />
-          Bar Marabu
-          <br />
-          Oppelner Str. 23
-          <br />
-          10997 Berlin
+          Bar John Doe
         </p>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d77700.80758076621!2d13.325355184115862!3d52.5126201408221!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e4d1b3d3071%3A0xce49bd7e20bc9095!2sMarabu!5e0!3m2!1sen!2sde!4v1700845394727!5m2!1sen!2sde"
+          src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d38875.59147889741!2d13.454425168395687!3d52.48412435348305!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sde!4v1707753880932!5m2!1sen!2sde"
           width="300"
           height="220"
           style={{ border: 0 }}
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyDo4kZwYfmcyAZcIvDTCMJli1NoMYl-kAk',
   authDomain: 'birthday-6cde2.firebaseapp.com',
